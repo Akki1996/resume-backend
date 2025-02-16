03eff00f239d782799f97914d29b1a63a6ffe791
mehul <mehulkaushal.sq@gmail.com>
2025-02-16 22:48:46 +0530

    fixes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const userRoutes = require('./routes/userRoutes')
 require('dotenv').config
 
 const app = express()
 app.use(express.json());
+app.use(cors({
+    origin: '*' 
+}));
 const PORT =process.env.PORT || 3000;
 
 mongoose.connect("mongodb+srv://akstrehan:gYFwCSN8YFs7KnOY@resumecluster.gqiok.mongodb.net/").then(()=>console.log("MongoDb connected"))
