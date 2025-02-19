3990a2c3b17aca7f450dc02f877f86eb977fc56a
akshay <akstrehan55@gmail.com>
2025-02-20 01:25:37 +0530

    resume templates

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -1,4 +1,6 @@
-const User = require("../models/Resume");
+const Resume = require("../models/Resume");
+const ejs = require("ejs");
+const path = require("path");
 
 const saveResumeData = async (req, res) => {
     try {
@@ -13,3 +15,52 @@ const saveResumeData = async (req, res) => {
       res.status(500).json({ success: false, message: "Failed to save resume", error: error.message });
     }
   };
+  
+  const sendResumeTemplates = async (req, res) => {
+      try {
+          const templatesDir = path.join(__dirname, "../resume-templates");
+          // const { id } = req.query;
+          const id ='67b62b39ea7c5c56a196c946'
+          let resumeData = {
+            personalDetails: {
+                name: "",
+                email: "",
+                phone: "",
+                address: ""
+            },
+            education: [],
+            professionalExperience: [],
+            skills: {
+                skills: [],
+                certifications: [],
+                languages: []
+            }
+        };
+
+        if (id) {
+           const fetchedData = await Resume.findOne({ id });
+          if (fetchedData) {
+              resumeData = fetchedData; 
+          }
+      }
+
+ 
+      const renderedTemplate1 = await ejs.renderFile(path.join(templatesDir, "template1.ejs"), { user: resumeData });
+      const renderedTemplate2 = await ejs.renderFile(path.join(templatesDir, "template2.ejs"), { user: resumeData });
+
+
+      res.json({
+          success: true,
+          templates: {
+              template1: renderedTemplate1,
+              template2: renderedTemplate2
+          }
+      });
+      } catch (error) {
+          console.error("Error rendering templates:", error);
+          res.status(500).json({ error: "Internal server error" });
+      }
+  };
+
+
+  module.exports ={saveResumeData,sendResumeTemplates}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {registerUser,LoginUser} =require('../controllers/userController')
-
+const {saveResumeData,sendResumeTemplates} = require('../controllers/resumeController')
 router.post('/register',registerUser)
 router.post('/login',LoginUser)
+router.post('/resumeData',saveResumeData)
+router.get('/resumeTemplates',sendResumeTemplates)
 
 module.exports = router;
